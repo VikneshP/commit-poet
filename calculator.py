f34c385ebafc3b01b12dff78bf712b34d1932b8b
Viknesh P <vikneshp@outlook.com>
2024-08-17 17:25:09 +0530

    feat: Add multiplication and division functionality to calculator This commit introduces new functionality to the calculator:
    - `multiply(num1: int, num2: int)`: Multiplies two integers.
    - `divide(num1: int, num2: int)`: Divides two integers.
    
    This enhancement expands the calculator\'s capabilities, making it more versatile.

--- calculator.py
+++ calculator.py
@@ -5,3 +5,11 @@ def add(num1: int, num2: int):
 
 def subtract(num1: int, num2: int):
     return num1 - num2
+
+
+def multiply(num1: int, num2: int):
+    return num1 * num2
+
+
+def divide(num1: int, num2: int):
+    return num1 / num2
